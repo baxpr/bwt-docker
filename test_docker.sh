4b95e7b22ad3d4179a2a9847f619d91f27d8fb40
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-07-17 14:42:46 -0500

    test

--- test_docker.sh
+++ test_docker.sh
@@ -4,8 +4,6 @@ docker run \
     --mount type=bind,src=$(pwd -P)/INPUTS,dst=/INPUTS \
     --mount type=bind,src=$(pwd -P)/OUTPUTS,dst=/OUTPUTS \
     bwt-docker:test \
-    run_matlab_entrypoint.sh \
-    /home/rogersbp/MATLAB/R2023a \
     WaveletDespike \
     /INPUTS/fmri.nii \
     /OUTPUTS/ctest
